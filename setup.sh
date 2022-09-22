9746202b6b015621b51e6b8013b49d8770796130
Pedoindo <107703144+Pedoindo@users.noreply.github.com>
2022-09-23 06:56:57 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,7 +20,7 @@ BURIQ () {
     done
     rm -f  /root/tmp
 }
-# https://raw.githubusercontent.com/Pedoindo/permission/main/ipmini 
+https://raw.githubusercontent.com/Pedoindo/permission/main/ipmini 
 MYIP=$(curl -sS ipv4.icanhazip.com)
 Name=$(curl -sS https://raw.githubusercontent.com/Pedoindo/permission/main/ipmini | grep $MYIP | awk '{print $2}')
 echo $Name > /usr/local/etc/.$Name.ini
